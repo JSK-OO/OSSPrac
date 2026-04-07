ba02d0c7a2a43026505d90a2bf02aecd230b358f
junseong Kim <zmdkgg12@dgu.ac.kr>
2026-04-07 23:53:23 +0900

    modify major

--- subject5/subject.cpp
+++ subject5/subject.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main() {
     string name;
     string numbers;
+    string major;
     cout << "이름을 입력하세요: ";
     cin >> name;
     cout << "학번을 입력하세요: ";
     cin >> numbers;
+    cout << "학과를 입력하세요: ";
+    cin >> major;
     cout << endl;
     cout << "<출력>" << endl;
     cout << "이름: " << name << endl;
     cout << "학번: " << numbers << endl;
+    cout << "학과: " << major << endl;
     return 0;
 }
