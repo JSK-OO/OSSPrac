30b005b46fe2acdab2970833b657ae78665dcc99
junseong Kim <zmdkgg12@dgu.ac.kr>
2026-04-09 17:46:25 +0900

    modify grade

--- subject5/subject.cpp
+++ subject5/subject.cpp
@@ -6,16 +6,20 @@ int main() {
     string name;
     string numbers;
     string major;
+    string grade;
     cout << "이름을 입력하세요: ";
     cin >> name;
     cout << "학번을 입력하세요: ";
     cin >> numbers;
     cout << "학과를 입력하세요: ";
     cin >> major;
+    cout << "학년을 입력하세요: ";
+    cin >> grade;
     cout << endl;
     cout << "<출력>" << endl;
     cout << "이름: " << name << endl;
     cout << "학번: " << numbers << endl;
     cout << "학과: " << major << endl;
+    cout << "학교: " << grade << endl;
     return 0;
 }
